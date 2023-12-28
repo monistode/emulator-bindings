ab8e23141727894eeb03f5144f23605d9f63e797
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-12-28 10:05:48 +0200

    Add bindings for processor listing

--- src/processors/mod.rs
+++ src/processors/mod.rs
@@ -6,6 +6,7 @@ pub mod cisc;
 pub mod stack;
 
 #[wasm_bindgen]
+#[derive(Clone)]
 pub enum ProcessorType {
     Stack,
     Cisc,
@@ -26,12 +27,32 @@ pub fn create_processor(processor_type: ProcessorType, binary: &[u8]) -> Box<dyn
     }
 }
 
+#[wasm_bindgen]
 pub struct ProcessorMetadata {
-    pub name: String,
-    pub description: String,
-    pub type_: ProcessorType,
+    name: String,
+    description: String,
+    type_: ProcessorType,
 }
 
+#[wasm_bindgen]
+impl ProcessorMetadata {
+    #[wasm_bindgen]
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    #[wasm_bindgen]
+    pub fn description(&self) -> String {
+        self.description.clone()
+    }
+
+    #[wasm_bindgen]
+    pub fn type_(&self) -> ProcessorType {
+        self.type_.clone()
+    }
+}
+
+#[wasm_bindgen]
 pub fn available_processors() -> Vec<ProcessorMetadata> {
     vec![
         ProcessorMetadata {
